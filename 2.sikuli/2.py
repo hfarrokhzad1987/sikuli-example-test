7c957c528476e7658e238672e810da30a19ff0d0
Houman <houman@houman-machine>
2012-10-16 22:21:12 -0600

    first upload

---
+++ 2.sikuli/2.py
@@ -0,0 +1,17 @@
+App.open("firefox")
+wait("aboutzstartn.png")
+doubleClick("aboutzstartn.png")
+type("hfviewer.appspot.com" + Key.ENTER)
+if (exists("meraterm.png")) :
+    click("meraterm.png")
+    type("cow" + Key.ENTER)
+    click("1349389480629.png")
+    wait("1349387752756.png")
+    for i in range(10):
+        wait(5)
+        click("1349387752756.png")
+    
+    
+     
+ 
+ 
